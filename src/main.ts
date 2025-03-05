efa57cfb9101aa8f973ebd8a3c4a38c501530590
rafal <you@example.com>
2025-03-05 22:09:51 +0100

    chore: refactor main

--- src/main.ts
+++ src/main.ts
@@ -2,44 +2,62 @@ import { transform } from 'typescript';
 import './maze/Maze';
 import { Maze, MazeNode } from './maze/Maze';
 import { Vec2d } from './types';
+import MazePathFinder from './maze/MazePathFinder';
 
-const maze = new Maze({
-    size: new Vec2d({ x: 5, y: 5 }),
-    initializer: new MazeNode({ isSolid: true })
-});
+const setupMaze = () => {
+    console.debug('initializer: ', MazeNode.initializer);
 
-// console.log(maze);
-
-[
-    [0, 0],
-    [1, 0],
-    [2, 0],
-    [0, 3],
-    [1, 4],
-    [0, 4],
-    [2, 1],
-    [2, 2],
-    [1, 2],
-    [0, 2],
-    [3, 1],
-    [4, 1],
-    [4, 0],
-    [2, 4],
-    [3, 4],
-    [4, 4],
-    [3, 3]
-].forEach(e => {
-    console.debug('main(): setting ', e);
-    const pos = new Vec2d({
-        x: e[0],
-        y: e[1]
+    const maze = new Maze({
+        size: new Vec2d({ x: 5, y: 5 }),
+        initializer: MazeNode.initializer
     });
 
-    maze.transformNode(pos, node => {
-        node.setIsSolid(false);
-        return node;
-    });
-});
+    [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [0, 3],
+        [1, 4],
+        [0, 4],
+        [2, 1],
+        [2, 2],
+        [1, 2],
+        [0, 2],
+        [3, 1],
+        [4, 1],
+        [4, 0],
+        [2, 4],
+        [3, 4],
+        [4, 4],
+        [3, 3]
+    ].forEach(e => {
+        const pos = new Vec2d({
+            x: e[0],
+            y: e[1]
+        });
 
-console.log(maze.toString());
+        maze.transformNode(pos, node => {
+            node.setIsSolid(false);
+            return node;
+        });
+    });
+    return maze;
+};
+
+// const mpf = new MazePathFinder(maze);
 // console.dir(maze, { depth: null });
+
+const findPath = (maze: Maze<MazeNode>) => {
+    // const mpf = new MazePathFinder(maze);
+    // console.debug(mpf);
+    // console.dir(mpf, { depth: null });
+};
+
+const main = () => {
+    const maze = setupMaze();
+    console.log(maze.toString());
+    findPath(maze);
+};
+export default main;
+
+main();
