b3f412f1763937e47719856e827f0dcb7c67cf95
rafal <you@example.com>
2025-03-05 09:21:51 +0100

    chore: make copy constructor in this shit lang

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
+import './maze/Maze';
+import { Maze, MazeNode } from './maze/Maze';
+
+const maze = new Maze({ x: 5, y: 3, initializer: new MazeNode({ isSolid: true }) });
+
+console.log(maze);
+
+maze.transformNode(1, 1, (node: MazeNode) => {
+    node.setIsSolid(false);
+    return node;
+});
+
+console.log(maze.toString());
+
 console.log('ziutek');
--- src/maze/Maze.ts
+++ src/maze/Maze.ts
@@ -1,45 +1,97 @@
-class MazeNode {
-    public constructor() {}
-
+export class MazeNode {
     private m_isSolid: boolean;
 
-    public setIsSolid(isSolid: boolean) {
+    constructor({ isSolid }: { isSolid: boolean }) {
         this.m_isSolid = isSolid;
     }
 
-    public isSolid() {
-        return this.isSolid;
+    setIsSolid(isSolid: boolean): void {
+        this.m_isSolid = isSolid;
+    }
+
+    isSolid(): boolean {
+        return this.m_isSolid;
     }
 }
 
 export class Maze<T extends MazeNode> {
-    public constructor(x?: number, y?: number, data?: T[][]) {
+    protected m_matrix: MazeNode[][];
+
+    protected x: number;
+    protected y: number;
+
+    /**
+     * @param x size of maze in horizontal direction
+     * @param y size of maze in vertical drection
+     * @param initializer value used to initialzie maze cells
+     * @param data maze matrix to initialze with
+     * @returns Maze
+     *
+     * @brief
+     * - If data is specified, all other parameters are ignored
+     * - If it is not x, y and initializer are used
+     */
+    public constructor({
+        x,
+        y,
+        initializer,
+        data
+    }: {
+        x?: number;
+        y?: number;
+        initializer?: T;
+        data?: T[][];
+    }) {
         if (data) {
             this.m_matrix = data;
             return;
         }
-        if (x && y) {
-            for (let i = 0; i < y; ++i) {
-                const tarr = Array.from({ length: x }, () => new MazeNode());
-                for (let j = 0; j < x; ++j) {}
-            }
+
+        if (x && y && initializer) {
+            this.x = x;
+            this.y = y;
+            this.m_matrix = Array.from(
+                // create vector
+                { length: y },
+                // cosisting of vectors
+                () =>
+                    Array.from(
+                        { length: x },
+                        // with value of initalizer
+                        () => {
+                            return Object.assign(
+                                Object.create(Object.getPrototypeOf(initializer)),
+                                initializer
+                            );
+                        }
+                    )
+            );
         }
     }
-
-    private m_matrix: MazeNode[][];
 
     public isSolid(x: number, y: number): boolean {
         return;
     }
 
-    public valueOf(): string {
+    public toString(): string {
         let str = '';
         this.m_matrix.forEach(e => {
             e.forEach(node => {
+                console.debug(node);
                 str += node.isSolid() ? '▓' : ' ';
             });
             str += '\n';
         });
         return str;
+    }
+
+    public transformNode(x: number, y: number, transform: (node: MazeNode) => MazeNode): void {
+        console.debug('transformNode');
+        const oldNode: MazeNode = this.m_matrix[x][y];
+        console.debug('old node', oldNode);
+        const node: MazeNode = transform(oldNode);
+        console.debug('transformed', oldNode);
+        this.m_matrix[y][x] = node;
+        console.debug(this.m_matrix);
     }
 }
