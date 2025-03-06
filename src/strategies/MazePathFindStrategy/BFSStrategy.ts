c8e54620f42951076560a87865fd71c0ead5d7ee
rafal <you@example.com>
2025-03-06 23:51:42 +0100

    feat: BFSstrategy w/o path reconstruction

--- src/strategies/MazePathFindStrategy/BFSStrategy.ts
+++ src/strategies/MazePathFindStrategy/BFSStrategy.ts
@@ -10,6 +10,42 @@ export class BFSStrategy<T extends MazePathFinderNode> implements MazePathFindSt
     findPath(maze: MazePathFinder<T>, start: Vec2d, end: Vec2d): MazePath {
         console.debug(`${BFSStrategy.name}::findPath()`);
 
+        const state: Map<string, { cameToFrom: Vec2d }> = new Map();
+
+        let queue = [start];
+
+        while (queue.length != 0) {
+            const currentNodePos = queue.shift();
+            // console.log(currentNodePos);
+            // console.debug('Current node', currentNodePos);
+            maze.getNode(currentNodePos).addLabels(['candidate']);
+
+            maze.getAdjacentNodes(currentNodePos).forEach(
+                ({ node: adjacientNode, pos: adjacientNodePos }) => {
+                    // console.debug('processing adjacient ', adjacientNodePos);
+                    if (
+                        // if node was already prcessed ignore it
+                        state.has(JSON.stringify(adjacientNodePos)) ||
+                        // or is colliding
+                        adjacientNode.isColliding()
+                    ) {
+                        return;
+                    }
+
+                    // save from where that node was reached
+                    state.set(JSON.stringify(adjacientNodePos), {
+                        cameToFrom: currentNodePos
+                    });
+
+                    // add it to queue
+                    queue = queue.concat(adjacientNodePos);
+                    adjacientNode.addLabels(['queued']);
+                }
+            );
+            console.log('_____________\n' + maze.toString());
+        }
+
+        console.debug(`${BFSStrategy.name}::findPath() done`);
         return [];
     }
 }
