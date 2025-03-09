831523a8fce9bf80170c9616ee5884a621c5f700
rafal <you@example.com>
2025-03-09 10:32:45 +0100

    feat(MazePathFinder): add obsevrer

--- src/maze/MazePathFinder.ts
+++ src/maze/MazePathFinder.ts
@@ -1,49 +1,52 @@
 import chalk from 'chalk';
 import { MazePathFindStrategy } from '../strategies/MazePathFindStrategy/MazePathFindStrategy';
 import { MazePath, Vec2d } from '../types';
-import { Maze } from './Maze';
-import { MazePathFinderNode, MazePathFinderNodeLabel } from './MazePathFinderNode';
+import { Maze, MazeConstructorArgs } from './Maze';
+import {
+    MazePathFinderNode,
+    MazePathFinderNodeLabel,
+    MPFNodeLabelCallBackParams,
+    MPFNodeLabelChangeCallback
+} from './MazePathFinderNode';
+import { call } from 'three/src/Three.TSL';
 
 /**
- *
+ * adds position
+ */
+type MPFLabelChangeCallbackParams = MPFNodeLabelCallBackParams & { pos: Vec2d };
+
+type MPFLabelChangeCallback = (args: MPFLabelChangeCallbackParams) => void;
+
+/**
+ * Extends Maze giving it pathfinding abilities
  */
 export default class MazePathFinder<T extends MazePathFinderNode> extends Maze<T> {
     protected m_pathfindStrategy: MazePathFindStrategy<T>;
 
-    onMPFNodeLabelChange({
-        node,
-        labelChanged
-    }: {
-        node: MazePathFinderNode;
-        labelChanged: MazePathFinderNodeLabel;
-    }): void {
-        let changedNodePos: Vec2d;
-        this.forEachNode(({ node: nodeNow, pos }) => {
-            if (nodeNow === node) {
-                // Corrected parentheses
-                changedNodePos = pos;
-            }
-        });
+    private m_MPFLabelChangeObservers: MPFLabelChangeCallback[] = [];
 
-        console.clear();
-        console.log(
-            `Changed node at ${JSON.stringify(changedNodePos)}, ${
-                node.hasLabel(labelChanged)
-                    ? chalk.green(`+ ${labelChanged}`)
-                    : chalk.red(`- ${labelChanged}`)
-            }\n${this.toString()}`
-        );
-
-        function msleep(n: number) {
-            Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
-        }
-        msleep(50);
+    public constructor(args: MazeConstructorArgs<T>) {
+        super(args);
+        this.addNodeChangeHandlers();
     }
 
+    /**
+     * @param strategy pathfinder strategy to use
+     */
     setPathFindStrategy(strategy: MazePathFindStrategy<T>): void {
         this.m_pathfindStrategy = strategy;
     }
 
+    /**
+     * Finds path from start to end
+     * @param start position of start
+     * @param end position of end
+     * @returns path
+     *
+     * @throws
+     * - when *start* or *end* position is invalid
+     * - when *start* or *end* is colliding
+     */
     findPath(start: Vec2d, end: Vec2d): MazePath {
         console.debug('MazePathFinder::findPath()');
         if (!this.m_pathfindStrategy) {
@@ -59,5 +62,36 @@ export default class MazePathFinder<T extends MazePathFinderNode> extends Maze<T
         this.getNode(start).makeStart();
         this.getNode(end).makeFinish();
         return this.m_pathfindStrategy.findPath(this, start, end);
+    }
+
+    /**
+     *
+     * @param callback function to call when any node in MazePathFinder changes
+     */
+    public addNodeLabelChangeObserver(callback: MPFLabelChangeCallback): void {
+        this.m_MPFLabelChangeObservers.push(callback);
+    }
+
+    protected invokeNodeLabelChangeObservers(params: MPFLabelChangeCallbackParams) {
+        this.m_MPFLabelChangeObservers.forEach(callback => {
+            callback(params);
+        });
+    }
+
+    /**
+     * adds listenters to nodes so that can callBack and notify maze about change in node with additional information of position they are at
+     */
+    private addNodeChangeHandlers(): void {
+        let changedNodePos: Vec2d;
+
+        this.forEachNode(({ node: nodeObserved, pos: nodeObserverdPos }) => {
+            const callback: MPFNodeLabelChangeCallback = ({ node, labelChanged }) => {
+                const nodePos: Vec2d = nodeObserverdPos;
+
+                this.invokeNodeLabelChangeObservers({ node, labelChanged, pos: nodePos });
+            };
+
+            nodeObserved.addLabelChangeObserver(callback);
+        });
     }
 }
